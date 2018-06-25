c076b0a6e1588aabbd698b4eb717aad9fe5ba699
Jery Hanks <jerryhanksokafor@gmail.com>
2018-06-25 21:30:42 +0100

    chore : re-add google api config after fixing build issues

--- app/src/main/java/me/jerryhanks/journalapp/ui/MainActivity.kt
+++ app/src/main/java/me/jerryhanks/journalapp/ui/MainActivity.kt
@@ -2,34 +2,43 @@ package me.jerryhanks.journalapp.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
 import me.jerryhanks.journalapp.R
+import me.jerryhanks.journalapp.ui.auth.AuthFragment
+import me.jerryhanks.journalapp.ui.entries.EntriesFragment
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        //check if user is logged in or not
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            gotoEntries()
+        } else {
+            gotoSignIn()
         }
+
     }
+
+    private fun gotoSignIn() {
+        val signInFragment = AuthFragment()
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, signInFragment)
+                .commitAllowingStateLoss()
+
+    }
+
+    private fun gotoEntries() {
+
+        val entriesFragment = EntriesFragment()
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, entriesFragment)
+                .addToBackStack(null)
+                .commitAllowingStateLoss()
+    }
+
 }
