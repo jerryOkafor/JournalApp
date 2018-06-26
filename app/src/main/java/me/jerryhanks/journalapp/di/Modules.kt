869e517f50cb1499da4b03e15538ca79d1e28cbb
Jery Hanks <jerryhanksokafor@gmail.com>
2018-06-26 09:53:08 +0100

    feat : provide GoogleSignInClient by di

--- app/src/main/java/me/jerryhanks/journalapp/di/Modules.kt
+++ app/src/main/java/me/jerryhanks/journalapp/di/Modules.kt
@@ -1,8 +1,12 @@
 package me.jerryhanks.journalapp.di
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import me.jerryhanks.journalapp.R
 import me.jerryhanks.journalapp.data.DataSource
 import me.jerryhanks.journalapp.data.Repository
 import me.jerryhanks.journalapp.ui.utils.NavigationUtils
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.applicationContext
 
@@ -13,7 +17,15 @@ import org.koin.dsl.module.applicationContext
  * @for JournalApp
  */
 
+
 val appModule: Module = applicationContext {
     bean { Repository() as DataSource }
     bean { NavigationUtils }
+    bean {
+        GoogleSignIn.getClient(androidApplication(),
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(androidApplication().getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build())
+    }
 }
--- app/src/main/java/me/jerryhanks/journalapp/ui/signIn/SignInFragment.kt
+++ app/src/main/java/me/jerryhanks/journalapp/ui/signIn/SignInFragment.kt
@@ -1,45 +1,34 @@
 package me.jerryhanks.journalapp.ui.signIn
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import kotlinx.android.synthetic.main.fragment_sign_in.*
-import me.jerryhanks.journalapp.R
-import android.content.Intent
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.fragment_sign_in.*
+import me.jerryhanks.journalapp.R
 import me.jerryhanks.journalapp.ui.entries.EntriesFragment
+import org.koin.android.ext.android.inject
 
 
 class SignInFragment : Fragment() {
 
-    private val googleSignInOptions by lazy {
-        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
-    }
-
     private val auth by lazy {
         FirebaseAuth.getInstance()
     }
-    private lateinit var googleSignInClient: GoogleSignInClient
+    private val googleSignInClient: GoogleSignInClient by inject()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        googleSignInClient = GoogleSignIn.getClient(requireContext(), googleSignInOptions)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
